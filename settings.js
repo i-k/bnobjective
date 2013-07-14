ca496b50b1aad3cfa79b53ff2140f691931cb881
Tuomas <tuomas.palenius@gmail.com>
2013-07-14 09:09:39 -0400

    Moved sensitive mongo settings to mongoSettings.js so settings.js can be checked into git

--- settings.js
+++ settings.js
@@ -1,12 +1,25 @@
 module.exports = new Settings()
 
 function Settings(){
-  this.appPort = 3002,
-  this.mongoHost = 'localhost',
-  this.bnAuthUrl = 'http://localhost:3000/api/auth',
+
+    // returns .mongoHost from the file given by the settings path
+    // returns orUseThisHost if the file was not found or if some other error occurred
+    function getMongoHostFromSettingsPathOr(pathToMongoSettings, orUseThisHost) {
+      var mongoHost;
+      try {
+        mongoHost = require(pathToMongoSettings).mongoHost;
+      } catch(err) {
+        console.log(err);
+      }
+      return mongoHost || orUseThisHost;
+    }
+
+  this.appPort = process.env.PORT,
+  this.mongoHost = getMongoHostFromSettingsPathOr('./mongoSettings.js', 'localhost'),
+  this.bnAuthUrl = 'http://bnauth.artiee.c9.io/api/auth',
   this.maxAmountOfTagsPerObjective = 10,
   this.maxTagLength = 50,
-  this.maxTagDescriptionLength = 20000,
-  this.maxTagNameLength = 200,
+  this.maxObjectiveDescriptionLength = 20000,
+  this.maxObjectiveNameLength = 200,
   this.maxEntryCommentsLength = 20000
 }
